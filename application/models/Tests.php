6fbafc4d7a1f551e587f971018d761fbc19745b5
Liu Yang <outcrop@163.com>
2018-12-24 14:26:39 +0800

    test new style

--- application/models/Tests.php
+++ application/models/Tests.php
@@ -44,7 +44,7 @@ class Tests extends CI_Model {
 			$reward=$this->getReward($blockheight+1);
 			$data['totalreward']=$data['totalreward']+$reward;
 			if($counter<101){
-				$data['activities']='<span class="label label-primary">Transaction</span>'; 
+				$data['activities']='<small class="label pull-right bg-blue">Transaction</small>'; 
 				$data['totalblocks'].="<tr><td>".$counter."</td><td><a href=/block/height/$blockheight>".$blockheight."</a></td><td>".$reward."</td><td>".$minedtime."</td></tr>";
 			}
 			}
@@ -104,7 +104,7 @@ class Tests extends CI_Model {
 		$row = $query->row();		
 		$data['transaction_count']=$row->count; 
 		if($data['transaction_count']>0){
-			$data['activities'].=' <span class="label label-success">Mining</span>'; 
+			$data['activities'].='  <small class="label pull-right bg-green">Mining</small>'; 
 			}
 		
 		$data['totalpage']=round($data['transaction_count']/$perpage,0);
@@ -123,7 +123,7 @@ class Tests extends CI_Model {
 		$query = $this->db->query($sql);
 		$row = $query->row();		
 		if($row->count>0){
-			$data['activities'].='  <small class="label pull-right bg-yellow">Genesis</small>'; 
+			$data['activities']=' <small class="label pull-right bg-yellow">Genesis</small>'.$data['activities']; 
 			}
 		
 		
