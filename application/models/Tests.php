2358b19408b0030587f0f25c7958d2baeae9ec9a
Liu Yang <outcrop@163.com>
2018-12-24 22:30:46 +0800

    test

--- application/models/Tests.php
+++ application/models/Tests.php
@@ -5,159 +5,15 @@ class Tests extends CI_Model {
 	
 	public function getNetworkStatus(){
 		$this->load->database();
-		//$timetag=(time()-(24*60*60))*1000; time>$timetag AND
-		$topminersql="select beneficiary,count(*) from miner WHERE orphan is FALSE group by beneficiary order by count desc;";
-		$query = $this->db->query($topminersql);
-		$counter=0;
-		$blockcounter=0;
-		$data['topminers']= "";
-		$data['lastmined']= "";
-		foreach ($query->result() as $row)
-		{
-			$counter++;
-			$blockcounter=$blockcounter+$row->count;
-			
-			if($counter<21){
-				$showaddress=$row->beneficiary;
-				$trueaddress=$showaddress;
-				$alias=$this->getalias($trueaddress);
-				if($showaddress==$alias){
-					$showaddress="ak_****".substr($showaddress,-4);
-				}else{
-					$showaddress=$alias;
-					}
-				$minedblocks=$row->count;
-				$percentage=round((($minedblocks*100)/$this->GetTopHeight()),2);
-				//<td>".$this->getTotalReward($trueaddress)." AE</td>
-				$data['topminers'].= "<tr><td>".$counter."</td><td><a href=/address/wallet/$trueaddress>".$showaddress."</a></td><td><span class='badge bg-blue'>".$minedblocks."</span></td><td>$percentage %</td><td>".$this->getTotalReward($trueaddress)." AE</td></tr>";
-			}
-		}
-
-		$data['blocksmined']= $blockcounter;
-		$data['totalminers']= $query->num_rows();
+		
+		$data['topheight']= $this->GetTopHeight();
 		$data['totalaemined']=$this->getTotalMined();
 		
-		////////////////////////////top 20 miners last 24h////////////////////////////////////////////
-		$timetag=(time()-(24*60*60))*1000; 
-		$blocksnum_24=0;
-		$getblockssql="SELECT count(*) FROM miner WHERE time>$timetag AND orphan is FALSE";
-		$query = $this->db->query($getblockssql);
-		$row = $query->row();
-		$blocksnum_24=$row->count;
-		$data['total_24']=$blocksnum_24;
 		
-		$topminersql="select beneficiary,count(*) from miner WHERE time>$timetag AND orphan is FALSE group by beneficiary order by count desc;";
-		$query = $this->db->query($topminersql);
-		$counter=0;
-		$blockcounter=0;
-		$data['topminers_24']= "";
-		$data['lastmined']= "";
-		$data['piechart']= "";
-		$data['totalhashrate']=0;
-		$piecounter=0;
-		foreach ($query->result() as $row)
-		{
-			$counter++;
-			$blockcounter=$blockcounter+$row->count;		
-				
-			if($counter<11){
-				$minedblocks=$row->count;
-				$showaddress=$row->beneficiary;
-				$trueaddress=$row->beneficiary;
-				$alias=$this->getalias($trueaddress);
-				if($showaddress==$alias){
-					$showaddress="ak_****".substr($showaddress,-4);
-				}else{
-					$showaddress=$alias;
-					}
-				
-				if($counter<7){
-					$piecounter=$piecounter+$minedblocks;
-					$data['piechart'].=' {label: "'.$showaddress.'('.round((($minedblocks*100)/$blocksnum_24),2).'%)'.'", value: '.$minedblocks.'},'."\n";
-					}
-				
-				
-				$percentage=round((($minedblocks*100)/$blocksnum_24),2);
-				if($counter==1){
-					$beehashrate=$this->getHashRate();
-					$data['totalhashrate']=round(($beehashrate[0]/$percentage)*10,2);}
-				//<td>".$this->getTotalReward($trueaddress)." AE</td>
-				$data['topminers_24'].= "<tr><td>".$counter."</td><td><a href=/address/wallet/$trueaddress>".$showaddress."</a></td><td><span class='badge bg-blue'>".$minedblocks."</span></td><td>$percentage%</td></tr>";
-			}
-		}
 		
-		$data['piechart'].=' {label: "else('.round(((($blocksnum_24-$piecounter)*100)/$blocksnum_24),2).'%)'.'", value: '.($blocksnum_24-$piecounter).'}';
-		
-		////////////////////////////////Latest 20 Transactions////////////////////////
-		$trans_sql="SELECT * from transactions order by block_height desc,nonce desc limit 20";		
-		$query = $this->db->query($trans_sql);
-		$data['lasttxs']="";
-		$counter=0;
-		foreach ($query->result() as $row){
-			$counter++;
-			$hash=$row->hash;
-			$showhash="th_****".substr($hash,-4);
-			$block_hash=$row->block_hash;
-			$show_block_hash="mh_****".substr($block_hash,-4);
-			$txtime=$this->getTxsTime($block_hash);
-			$sender_id=$row->sender_id;
-			$alias=$this->getalias($sender_id);
-			
-			
-			if($alias!=$sender_id){
-				$show_sender_id=$alias;
-			}else{
-				$show_sender_id="ak_****".substr($sender_id,-4);
-				}
-				
-				
-			$recipient_id=$row->recipient_id;
-			$alias=$this->getalias($recipient_id);			
-			if($alias!=$recipient_id){
-				$show_recipient_id=$alias;
-			}else{
-				$show_recipient_id="ak_****".substr($recipient_id,-4);
-				}
-			
-			//$show_recipient_id="ak_****".substr($recipient_id,-4);
-			$amount=$row->amount;
-			$amount=round($amount/1000000000000000000,2);
-			//$data['lasttxs'].="<tr><td>$counter</td><td>$showhash</td><td>$amount</td><td><a href=/address/wallet/$sender_id>$show_sender_id</a></td><td><a href=/address/wallet/$recipient_id>$show_recipient_id</a></td><td>$txtime</td></tr>";
-			$data['lasttxs'].="<tr><td>$counter</td><td>$amount</td><td><a href=/address/wallet/$sender_id>$show_sender_id</a></td><td><a href=/address/wallet/$recipient_id>$show_recipient_id</a></td><td><a href=/block/transaction/$hash>$showhash</a></td><td>$txtime</td></tr>";
-
-			}
-			
-		
-		/////////////////////////////////Last 20 blocks/////////////////////////
-		$counter=0;
-		$query = $this->db->query('select beneficiary,height,time from miner WHERE orphan is FALSE order by height desc LIMIT 20;');
-		foreach ($query->result() as $row)
-		{			
-			$counter++;
-			$millisecond=$row->time;
-			$millisecond=substr($millisecond,0,strlen($millisecond)-3); 
-			$whenmined=time()-$millisecond;
-			//$minedtime=$whenmined;
-			$minedtime=date('i:s',$whenmined);
-			//$showaddress=$this->strMiddleReduceWordSensitive ($row->beneficiary, 30);
-			$showaddress=$row->beneficiary;
-			$trueaddress=$row->beneficiary;
-			$alias=$this->getalias($trueaddress);
-				if($showaddress==$alias){
-					$showaddress="ak_****".substr($showaddress,-4);
-				}else{
-					$showaddress=$alias;
-					}
-			$height=$row->height;
-			if($this->notOrphan($height)){
-				$data['lastmined'].="<tr><td> <a href=/block/height/$height>$height</a></td><td><a href=/address/wallet/$trueaddress>".$showaddress."</a></td><td>".$minedtime."</td><td><span class='badge bg-green'>Normal</span></td></tr>";			
-			}else{
-				$data['lastmined'].="<tr><td><a href=/block/height/$height>$height</a></td><td><a href=/address/wallet/$trueaddress>".$showaddress."</a></td><td>".$minedtime."</td><td><span class='badge bg-yellow'>Forked</span></td></tr>";				
-				}
-		}
 		
 		//////////////////////////////get difficulty////////////////////////////
-		$url="http://52.77.168.79:3013/v2/status";
+		$url="http://127.0.0.1:3013/v2/status";
 		$websrc=$this->getwebsrc($url);
 		$data['peer_count']=0;
 		if(strpos($websrc,"difficulty")>0){
@@ -166,8 +22,7 @@ class Tests extends CI_Model {
 			$data['difficulty']=$match[1];
 			$data['difficultyfull']=$data['difficulty'];
 			//$data['difficulty']=round($data['difficulty']/10000000000,2);
-			$data['difficulty']=round($data['difficulty']/16777216/1000,0)." K";
-			
+			$data['difficulty']=round($data['difficulty']/16777216/1000,0)." K";			
 			$data['peer_count']=$match[6];
 		}
 		
