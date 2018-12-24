c706dc51328de5155ff933b623a86e20e93828e8
Liu Yang <outcrop@163.com>
2018-12-24 21:30:37 +0800

    test

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -122,7 +122,7 @@ class Test extends CI_Controller {
 		}
 		
 	public function network(){
-		$this->load->model('test');
+		$this->load->model('tests');
 		$data=$this->tests->getNetworkStatus();
 		$this->load->view('networkstatus.html',$data);
 		}	
