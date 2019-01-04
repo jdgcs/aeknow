29df601995f0a34f6ac4448ab4d4c08b8336a339
Liu Yang <outcrop@163.com>
2019-01-04 23:02:52 +0800

    fix data

--- application/controllers/Aens.php
+++ application/controllers/Aens.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Aens extends CI_Controller {
 	public function index()
 	{	
-		$this->load->view('aens.html',$data);
+		$this->load->view('aens.html');
 	}
 	
 	function query($aename){
