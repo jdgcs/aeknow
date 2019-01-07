d41347ab0f707eaf9d44b54a6fe3924d0d6b52d5
Liu Yang <outcrop@163.com>
2019-01-07 21:04:48 +0800

    session

--- application/controllers/Aens.php
+++ application/controllers/Aens.php
@@ -12,7 +12,10 @@ class Aens extends CI_Controller {
 	function query($aename=""){
 		$this->load->model('aenses');		 
 		$data=$this->aenses->query($aename);
-		if($data['status']=="available"){			
+		if($data['status']=="available"){	
+			$this->load->library('session');
+			$this->session->set_userdata('aename',$aename);
+			$this->session->set_userdata('querytime',time());		
 			$this->load->view('reg.html',$data);
 		}else{
 			$this->load->view('aens.html',$data);
@@ -24,14 +27,14 @@ class Aens extends CI_Controller {
 		$akaddress=$this->input->post('akaddress');
 		
 		$this->load->library('session');
-		$this->session->set_userdata('aename',$aename);
+		//$this->session->set_userdata('aename',$aename);
 		$this->session->set_userdata('akaddress',$akaddress);
 		
 		echo "Sessid:".$this->session->session_id;
 		echo "<br/>Active:".$this->session->last_activity;
 		echo "<br/>Name:".$this->session->userdata('aename');;
 		echo "<br/>Address:".$this->session->userdata('akaddress');;
-			
+		echo "<br/>Address:".$this->session->userdata('querytime');;	
 		
 		
 		//echo "$aename:$akaddress recorded.";
