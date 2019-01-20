280ff95f0597cce89cb5e46d9266a6e13299efd8
Liu Yang <outcrop@163.com>
2019-01-20 12:33:35 +0800

    add stats channel page

---
+++ application/controllers/Stats.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Stats extends CI_Controller {
+
+	public function index()
+	{	
+		$this->load->view('stats.html');
+	}
+	
+	
+
+}
+
