e5a5a8294cd1e431de75b0e1a2030319e0058b3a
Liu Yang <outcrop@163.com>
2019-01-21 10:21:39 +0800

    addd f2pool

--- application/models/Stat.php
+++ application/models/Stat.php
@@ -6,6 +6,8 @@ class Stat extends CI_Model {
 		public function getHashrate(){
 			$data['title']="Aeternity Mining Hashrate";		
 			$data['tabledata']='{"period": "2018-12-28 17:55:47", "hashrate": 3186}';
+			$data['tabledata_f2']='{"period": "2018-12-28 17:55:47", "hashrate_f2": 1046}';
+			
 			$this->load->database();
 			$nowtime=time();
 			$step=round(($nowtime-1545990947)/100,0);
@@ -17,6 +19,14 @@ class Stat extends CI_Model {
 				$hashrate= $row->hashrate;
 				$updatetime=date("Y-m-d H:i:s",$row->updatetime);
 				$data['tabledata'].=',{"period": "'.$updatetime.'", "hashrate":'.$hashrate.'}';
+				
+				$sql="SELECT hashrate,updatetime from pools WHERE poolname='f2pool' AND updatetime > ".($nowtime-(100-$i)*$step) ." ORDER BY pid ASC LIMIT 1";
+				$query = $this->db->query($sql);
+				$row = $query->row();
+				$hashrate= $row->hashrate;
+				$updatetime=date("Y-m-d H:i:s",$row->updatetime);
+				$data['tabledata_f2'].=',{"period": "'.$updatetime.'", "hashrate_f2":'.$hashrate.'}';
+				
 				}
 			
 			return $data;
