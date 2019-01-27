c064a0418a1c0f440a6787feb76bacb081f85de3
aedev <outcrop@163.com>
2019-01-27 22:13:42 +0800

    renmae to txs

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -18,7 +18,7 @@ class Test extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
+	public function txs()
 	{	
 		$this->load->model('tests');
 		$data=$this->tests->getTransactions(1);
