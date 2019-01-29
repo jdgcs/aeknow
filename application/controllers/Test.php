fff6ba7dc25f10cc126b3ba69c9b4e510ccd9e8b
aedev <outcrop@163.com>
2019-01-29 12:36:24 +0800

    tst pagination

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -111,10 +111,10 @@ class Test extends CI_Controller {
 		echo $this->Tests->getTotalMined()."<br/>";
 		echo print_r($this->Tests->getHashRate());
 		}
-	public function wallet($ak=NULL,$page=1){		
+	public function wallet($ak=NULL,$page=1,$type='all'){		
 		if($page<1){$page=1;}
 		$this->load->model('tests');	
-		$data=$this->tests->getWalletInfo($ak,$page);
+		$data=$this->tests->getWalletInfo($ak,$page,$type);
 		$this->load->view('account.html',$data);
 		//$this->output->cache(2);
 		}
