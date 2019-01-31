b0c58bcacd15f847ef09d75c0086009aed6bb4d6
aedev <outcrop@163.com>
2019-01-31 19:51:46 +0800

    try router

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,4 +52,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'miner';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['/(:any).test'] = "name/show/$1.test";
+$route['/(:any).test'] = "/name/show/$1.test";
