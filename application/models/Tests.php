82cb37a900b51645d3383961cac648f18893e1c6
aedev <outcrop@163.com>
2019-03-02 17:07:44 +0800

    fix

--- application/models/Tests.php
+++ application/models/Tests.php
@@ -8,7 +8,7 @@ class Tests extends CI_Model {
 		$topheight=$this->GetTopHeight();
 		//$timetag=(time()-(24*60*60))*1000; time>$timetag AND
 		//$topminersql="select beneficiary,count(*) from miner WHERE orphan is FALSE group by beneficiary order by count desc;";
-		$topminersql="select data->'beneficiary' as beneficiary,count(*) from keyblocks WHERE orphan is NULL group by beneficiary order by count desc;";
+		$topminersql="select data->>'beneficiary' as beneficiary,count(*) from keyblocks WHERE orphan is NULL group by beneficiary order by count desc;";
 		$query = $this->db->query($topminersql);
 		$counter=0;
 		$blockcounter=0;
@@ -20,7 +20,7 @@ class Tests extends CI_Model {
 			$blockcounter=$blockcounter+$row->count;
 			
 			if($counter<21){
-				$showaddress=$row->>beneficiary;
+				$showaddress=$row->beneficiary;
 				$trueaddress=$showaddress;
 				$alias=$this->getalias($trueaddress);
 				if($showaddress==$alias){
