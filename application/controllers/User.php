1b21ebcc8eab43363c9593760594fed9f258241f
aedev <outcrop@163.com>
2019-04-22 21:57:38 +0800

    fix class name

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Aens extends CI_Controller {
+class User extends CI_Controller {
 	public function index()
 	{	
 		//default user page
