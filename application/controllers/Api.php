8c97c3253515f4e2b7e452c9115847cb7a4ba272
aedev <outcrop@163.com>
2019-05-15 20:09:00 +0800

    use api

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -24,7 +24,7 @@ class Api extends CI_Controller {
 		}
 	
 	public function network(){
-		$this->load->model('apis');
+		$this->load->model('networks');
 		$data['api']=$this->apis->getNetworkStatus();
 		$this->load->view('en/api.html',$data);
 		$this->output->cache(1/4);
