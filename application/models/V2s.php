7a4e237b0b1dc0537d9f2e8e6055adc630e61382
aedev <outcrop@163.com>
2019-05-19 10:16:59 +0800

    add module

---
+++ application/models/V2s.php
@@ -0,0 +1,175 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class V2s extends CI_Model {
+	
+	public function postTransaction($tx){
+		$tx=urldecode($tx);
+		$data['result']="";
+		
+		$jsonStr ='{ "tx": "'.$tx.'"}';	
+		$pubnode="http://127.0.0.1:3013/v2/transactions";
+		$return= $this->http_post_json($pubnode, $jsonStr); 
+		
+		if($return[0]==200){
+				$info=json_decode($return[1]);
+				$txhash=$info->tx_hash;
+				$data['result']="Successful: <a href=/block/transaction/$txhash>$txhash</a>";
+			}else{	
+				$result=$return[1];
+				$data['result']="Failed:$result";
+			}
+		
+		return $data;
+		}
+		
+	public function postTx($jsonStr){
+		$pubnode="http://127.0.0.1:3013/v2/transactions";
+		$return= $this->http_post_json($pubnode, $jsonStr); 
+		return $return[1];
+		}	
+	
+	public function getalias($address){
+		$this->load->database();
+		$sql="SELECT alias from addressinfo WHERE address='$address' limit 1";
+		$query = $this->db->query($sql);
+		$row = $query->row();		
+		
+		if($query->num_rows()>0){
+			//echo  $row->alias;
+			return $row->alias;
+			}
+		return $address;
+		}
+		
+	private function getTransactionTime($block_hash){
+		$this->load->database();
+		$totalmins=0;
+		$sql="SELECT time from microblock WHERE hash='$block_hash' limit 1";
+		$query = $this->db->query($sql);
+		$row = $query->row();
+		if($query->num_rows()>0){
+			$totalmins=round(($row->time/1000),0);
+		}
+		return date("Y-m-d H:i:s",$totalmins);	
+		}	
+	
+	
+	
+	public function getBlockHeight($keyblockhash){
+		$this->load->database();
+		$sql="SELECT * from miner WHERE hash='$keyblockhash' AND orphan is FALSE";
+		$query = $this->db->query($sql);
+		if($query->num_rows()==0){return -1;}
+		$row = $query->row();
+		return $row->height;
+		}	
+	
+	public function getPow($height){
+		$this->load->database();
+		$sql="SELECT pow from miner WHERE height=$height AND orphan is FALSE";
+		$query = $this->db->query($sql);
+		if($query->num_rows()==0){return "No pow info of $height";}
+		$row = $query->row();
+		return $row->pow;
+		}
+	private function getMicroBlockTransNum_old($mhash){
+		$this->load->database();
+		$sql="SELECT count(*) from transactions WHERE block_hash='$mhash'";
+		$query = $this->db->query($sql);
+		$row = $query->row();
+		return $row->count;
+		}
+		
+	public function getMicroBlockTransNum($mhash){
+		$url=DATA_SRC_SITE."v2/micro-blocks/hash/$mhash/transactions/count";
+		$websrc=$this->getwebsrc($url);
+		if(strpos($websrc,"count")>0){
+			$pattern='/{"count":(.*)}/i';
+			preg_match($pattern,$websrc, $match);
+			//echo  $match[1];
+			return $match[1];
+		}
+		return 0;
+		}
+	private function getMicroBlocksNum($height){
+		$this->load->database();
+		$sql="SELECT count(*) from microblock WHERE height=$height";
+		$query = $this->db->query($sql);
+		$row = $query->row();
+		return $row->count;
+		}
+	private function getReward($blockheight){
+		$blockheight=$blockheight+1;
+		$this->load->database();
+		$sql="SELECT reward from aeinflation WHERE blockid<$blockheight ORDER BY blockid desc LIMIT 1";
+		$query = $this->db->query($sql);
+		$row = $query->row();
+		return $row->reward/10;
+		}
+	
+public function http_post_json($url, $jsonStr)
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonStr);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+				'Content-Type: application/json; charset=utf-8',
+				'Content-Length: ' . strlen($jsonStr)
+			)
+		);
+		$response = curl_exec($ch);
+		$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
+	 
+		return array($httpCode, $response);
+	}
+
+
+private function getwebsrc($url) {
+	$curl = curl_init ();
+	$agent = "User-Agent: AE-testbot";
+	
+	curl_setopt ( $curl, CURLOPT_URL, $url );
+
+	curl_setopt ( $curl, CURLOPT_USERAGENT, $agent );
+	curl_setopt ( $curl, CURLOPT_ENCODING, 'gzip,deflate' );
+	curl_setopt ( $curl, CURLOPT_FOLLOWLOCATION, 1 ); //×¥È¡301Ìø×ªºóÍøÖ·
+	curl_setopt ( $curl, CURLOPT_AUTOREFERER, true );
+	curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, 1 );
+	curl_setopt ( $curl, CURLOPT_TIMEOUT, 60 );
+	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+	
+	$html = curl_exec ( $curl ); // execute the curl command
+	$response_code = curl_getinfo ( $curl, CURLINFO_HTTP_CODE );
+	if ($response_code != '200') { //Èç¹ûÎ´ÄÜ»ñÈ¡¸ÃÒ³Ãæ£¨·Ç200·µ»Ø£©£¬ÔòÖØÐÂ³¢ÊÔ»ñÈ¡
+	//	echo 'Page error: ' . $response_code . $html;	
+		$html='Page error: ' . $response_code.$html;
+	} 
+	curl_close ( $curl ); // close the connection
+
+	return $html; // and finally, return $html
+}
+
+
+function GetTopHeight()	{
+	$url=DATA_SRC_SITE."v2/blocks/top";
+	$websrc=$this->getwebsrc($url);
+	$info=json_decode($websrc);
+	if(strpos($websrc,"key_block")==TRUE){		
+		return $info->key_block->height;
+	}
+		
+	if(strpos($websrc,"micro_block")==TRUE){
+		return $info->micro_block->height;
+		}
+	
+	return 0;
+	}
+	
+	
+
+}
