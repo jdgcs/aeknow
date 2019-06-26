d23b5ada5eb66698625ba23c226280f1b84b86b7
aedev <outcrop@163.com>
2019-06-26 11:14:53 +0800

    fix

--- application/models/Wallets.php
+++ application/models/Wallets.php
@@ -48,7 +48,7 @@ class Wallets extends CI_Model {
 			if($counter<101){				
 				$data['totalblocks'].="<tr><td>".$counter."</td><td><a href=/block/height/$blockheight>".$blockheight."</a></td><td>".$reward."</td><td>".$minedtime."</td></tr>";
 			}
-			}*/
+			}
 			
 		$sql= "select height,data FROM keyblocks WHERE benifit='$ak' AND orphan is NULL order by hid desc LIMIT 100";
 		$query = $this->db->query($sql);
@@ -63,7 +63,7 @@ class Wallets extends CI_Model {
 			$data['activities']=' <a class="pull-right"> &nbsp; <span class="badge bg-blue">Mining</span></a>'; 
 			$counter++;
 			$blockheight=$row->height;
-			$info=json_decode($row->data);
+			$onfo=json_decode($row->data);
 			$millisecond =$info->time;
 			$millisecond=substr($millisecond,0,strlen($millisecond)-3); 
 			$minedtime=date("Y-m-d H:i:s",$millisecond);
@@ -72,7 +72,7 @@ class Wallets extends CI_Model {
 			if($counter<101){				
 				$data['totalblocks'].="<tr><td>".$counter."</td><td><a href=/block/height/$blockheight>".$blockheight."</a></td><td>".$reward."</td><td>".$minedtime."</td></tr>";
 			}
-			}
+			}*/
 		/////////////////////////////////////////////get Transactions//////////////////////////////////
 		$ok=str_replace("ak_","ok_",$ak);
 		
