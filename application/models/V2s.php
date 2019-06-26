e860645b7c5c0ade41f4ec094a0e434e8d3df6c6
aedev <outcrop@163.com>
2019-06-26 12:05:36 +0800

    clean return

--- application/models/V2s.php
+++ application/models/V2s.php
@@ -6,11 +6,12 @@ class V2s extends CI_Model {
 		if($type=="oracles" && $function=="query"){
 			$url = "http://localhost:3113/v2/debug/oracles/query";
 			$return= $this->http_post_json($url, $poststr);  
-			print_r($return);
-			$keys=json_decode($return[1]);  
+			echo $return[1];
+			//print_r($return);
+			//$keys=json_decode($return[1]);  
 	
-			$tx_unsigned=$keys->tx;
-			echo "$poststr<br />$tx_unsigned<br />";
+			//$tx_unsigned=$keys->tx;
+			//echo "$poststr<br />$tx_unsigned<br />";
 			}
 		}
 	
