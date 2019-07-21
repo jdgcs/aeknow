d90ee66ab06a540498107ef57ed403d174585a5f
aedev <outcrop@163.com>
2019-07-21 21:59:24 +0800

    ready for the prediction market

--- application/controllers/Oracle.php
+++ application/controllers/Oracle.php
@@ -2,21 +2,30 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Oracle extends CI_Controller {
-	public function index()
-	{	
+	public function index(){	
+		//need to be re-demonstration
 		$this->load->model('oracles');	
 		$data=$this->oracles->getOracleList();
 		$this->load->view('en/oracles_index.html',$data);
 		$this->output->cache(5);
 	}
 		
-	public function id($oracle_id="")
-	{	
+	public function id($oracle_id=""){	
+		//show the deitail of oracle_id with ok_
 		$this->load->model('oracles');	
 		$data=$this->oracles->getOracleDetail($oracle_id);
 		$this->load->view('en/oracles_detail.html',$data);
 		$this->output->cache(1);
 	}
+	
+	public function show($ak){
+		//show all the oracles of the address ak_
+		}
+	
+	public function detail($txhash){
+		//show the dynamic details of a single onchain prediction
+		
+		}
 	
 }
 
