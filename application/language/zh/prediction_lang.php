79a81d166d6d0b5f4117141816b43eb2ae0c91df
aedev <outcrop@163.com>
2019-07-24 10:28:26 +0800

    add realttime results title

--- application/language/zh/prediction_lang.php
+++ application/language/zh/prediction_lang.php
@@ -6,6 +6,7 @@ $lang['pre_options'] = '预测选项';
 $lang['pre_participate'] = '参与方式';
 $lang['pre_guide'] = '转账小数点后第一位为选项，如3.2表示选项2。';
 $lang['pre_returnrate'] = '返还率';
+$lang['pre_rtprediction']='实时预测结果';
 
 
 
