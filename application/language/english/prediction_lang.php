149d88a2fd95863519fc15a906a6d39804af384c
aedev <outcrop@163.com>
2019-07-24 09:48:43 +0800

    add English & Chinese

--- application/language/english/prediction_lang.php
+++ application/language/english/prediction_lang.php
@@ -1,5 +1,5 @@
 <?php
 $lang['pre_query'] = 'Query';
 $lang['pre_description'] = 'Description';
-$lang['pre_options'] = 'Description';
+$lang['pre_options'] = 'Predict Options';
 
--- application/language/zh/prediction_lang.php
+++ application/language/zh/prediction_lang.php
@@ -1,3 +1,4 @@
 <?php
 $lang['pre_query'] = '查询';
 $lang['pre_description'] = '简介';
+$lang['pre_options'] = '预测选项';
