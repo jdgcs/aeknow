a18d093a17b373403dbeecb0e6be9aece97a056d
aedev <outcrop@163.com>
2019-07-24 10:10:16 +0800

    add details

--- application/language/english/prediction_lang.php
+++ application/language/english/prediction_lang.php
@@ -4,3 +4,5 @@ $lang['pre_description'] = 'Description';
 $lang['pre_options'] = 'Predict Options';
 $lang['pre_participate'] = 'How to participate';
 $lang['pre_guide'] = 'The first decimal point is the option, for example, 3.2 represents option 2.';
+$lang['pre_returnrate'] = 'Return Rate';
+
