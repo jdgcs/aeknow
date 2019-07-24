c4906d2c02beb19f2e598c02e1521fbdecd52c30
aedev <outcrop@163.com>
2019-07-24 10:00:37 +0800

    add brief guide

--- application/language/english/prediction_lang.php
+++ application/language/english/prediction_lang.php
@@ -2,4 +2,5 @@
 $lang['pre_query'] = 'Query';
 $lang['pre_description'] = 'Description';
 $lang['pre_options'] = 'Predict Options';
-
+$lang['pre_participate'] = 'How to participate';
+$lang['pre_guide'] = 'The first decimal point is the option, for example, 3.2 represents option 2.';
--- application/language/zh/prediction_lang.php
+++ application/language/zh/prediction_lang.php
@@ -2,3 +2,7 @@
 $lang['pre_query'] = '查询';
 $lang['pre_description'] = '简介';
 $lang['pre_options'] = '预测选项';
+$lang['pre_participate'] = '参与方式';
+$lang['pre_guide'] = '转账小数点后第一位为选项，如3.2表示选项2。';
+
+
