478b496e2b0cb7ad4b23a738fc9a7afe7c177a2a
aedev <outcrop@163.com>
2019-07-24 09:44:17 +0800

    use table as demonstration

--- application/language/english/prediction_lang.php
+++ application/language/english/prediction_lang.php
@@ -1,3 +1,5 @@
 <?php
+$lang['pre_query'] = 'Query';
+$lang['pre_description'] = 'Description';
+$lang['pre_options'] = 'Description';
 
-$lang['oracle_query'] = 'Query';
--- application/language/zh/prediction_lang.php
+++ application/language/zh/prediction_lang.php
@@ -1,3 +1,3 @@
 <?php
-
-$lang['oracle_query'] = '查询';
+$lang['pre_query'] = '查询';
+$lang['pre_description'] = '简介';
