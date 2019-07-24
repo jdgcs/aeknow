d0cc8a92fe8e491c09db76acfa23b891299835bf
aedev <outcrop@163.com>
2019-07-24 10:29:55 +0800

    try to fix English title

--- application/language/english/prediction_lang.php
+++ application/language/english/prediction_lang.php
@@ -6,4 +6,4 @@ $lang['pre_options'] = 'Predict Options';
 $lang['pre_participate'] = 'How to participate';
 $lang['pre_guide'] = 'The first decimal point is the option, for example, 3.2 represents option 2.';
 $lang['pre_returnrate'] = 'Return Rate';
-$lang['pre_rtprediction']='Realtime Prediction Results';
+$lang['pre_rtprediction']='Realtime Prediction';
