7206b96b5ae1dd4e6eb9e6e9596bb49af2f1e5e8
aedev <outcrop@163.com>
2019-08-05 17:33:36 +0800

    add lang file for multi-languages

---
+++ application/language/en/index_lang .php
@@ -0,0 +1,15 @@
+<?php
+$lang['index_wallets'] = 'Wallets';
+$lang['index_mining'] = 'Mining';
+$lang['index_trading'] = 'Trading';
+$lang['index_contact'] = 'Contact';
+$lang['index_donate'] = 'Donate';
+$lang['index_search'] = 'Search';
+$lang['index_price'] = 'Price';
+$lang['index_latestblock'] = 'Latest Block';
+$lang['index_transactions'] = 'Transactions';
+$lang['index_marketcap'] = 'Market Cap';
+
+
+$lang['index_'] = '';
+
