ce21fafbfc5402395d83c8d80e5c5f620ccbf479
aedev <outcrop@163.com>
2019-08-06 16:37:02 +0800

    use the right file name

---
+++ application/language/zh/index_lang.php
@@ -0,0 +1,56 @@
+<?php
+//////////////////////////////////Header//////////////////////////////////////
+$lang['index_wallets'] = 'Wallets';
+$lang['index_trading'] = 'Trading';
+$lang['index_contact'] = 'Contact';
+
+//////////////////////////////////Index page//////////////////////////////////////
+$lang['index_donate'] = 'Donate';
+$lang['index_search'] = 'Search';
+$lang['index_price'] = 'Price';
+$lang['index_latestblock'] = 'Latest Block';
+$lang['index_transactions'] = 'Transactions';
+$lang['index_maxtps'] = 'Max TPS';
+$lang['index_viewmaxtps'] = 'Explore the details of the MAX tps';
+$lang['index_marketcap'] = 'Market Cap';
+$lang['index_mining'] = 'Mining';
+$lang['index_pools'] = 'Pools';
+$lang['index_explorerinflationcurve'] = 'Explore æternity inflation curve';
+$lang['index_oracles'] = 'Oracles';
+$lang['index_active'] = 'active';
+$lang['index_total'] = 'Total';
+$lang['index_contracts'] = 'Contracts';
+$lang['index_totalcontracts'] = 'Total Contracts';
+$lang['index_blocks'] = 'Blocks';
+$lang['index_miner'] = 'Miner';
+$lang['index_times'] = 'Time(S)';
+$lang['index_status'] = 'Status';
+$lang['index_amount'] = 'Amount';
+$lang['index_sender'] = 'Sender';
+$lang['index_receiver'] = 'Receiver';
+$lang['index_txtype'] = 'TxType';
+$lang['index_time'] = 'Time';
+$lang['index_stats'] = 'Stats';
+$lang['index_desofaens'] = 'Distributed naming system based on Aeternity.';
+$lang['index_desoforacles'] = 'Connecting real world with blockchain.';
+$lang['index_desofnodes'] = 'Full æternity nodes.';
+$lang['index_desofaccounts'] = 'Total active accounts.';
+$lang['index_totalaccounts'] = 'Total Accounts';
+$lang['index_desofaepp'] = 'Applications based on æternity.';
+$lang['index_desofapi'] = 'API of aeknow.';
+$lang['index_top500'] = 'Wealth 500';
+$lang['index_desoftop500'] = 'Wealth 500 of æternity(Mainnet).';
+$lang['index_parteners'] = 'Parteners';
+$lang['index_sponsors'] = 'Sponsors';
+//////////////////////////////////Side bar//////////////////////////////////////
+$lang['index_dashboards'] = 'Dashboards';
+$lang['index_foruser'] = 'For User';
+$lang['index_forminer'] = 'For Miner';
+$lang['index_fordeveloper'] = 'For Developer';
+$lang['index_news'] = 'News';
+$lang['index_datatracking'] = 'Data Tracking';
+$lang['index_networkstatus'] = 'Network Status';
+$lang['index_popularstats'] = 'Popular Stats';
+$lang['index_links'] = 'Links';
+$lang['index_'] = '';
+
