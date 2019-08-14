93e82859b992e0b233ef008f569c552e7af08af2
aedev <outcrop@163.com>
2019-08-14 12:09:10 +0800

    use the unique language file

--- application/language/en/index_lang.php
+++ application/language/en/index_lang.php
@@ -63,5 +63,29 @@ $lang['index_desoftop500'] = 'Wealth 500 of æternity(Mainnet).';
 $lang['index_parteners'] = 'Parteners';
 $lang['index_sponsors'] = 'Sponsors';
 
-//////////////////////////////////Side bar//////////////////////////////////////
+//////////////////////////////////Prediction Market//////////////////////////////////////
+$lang['pre_query'] = 'Query';
+$lang['pre_oracle'] = 'Oracle';
+$lang['pre_range'] = 'Range';
+$lang['pre_description'] = 'Description';
+$lang['pre_options'] = 'Predict Options';
+$lang['pre_participate'] = 'How to participate';
+$lang['pre_guide'] = 'The first decimal point is the option, for example, 3.2 AE represents option 2 with 3.2 AE.';
+$lang['pre_returnrate'] = 'Return Rate';
+$lang['pre_rtprediction']='Realtime Prediction';
+$lang['pre_init']='Init';
+$lang['pre_total']='Total';
+$lang['pre_rate']='Rate';
+$lang['pre_effectivetokens']='Effective Tokens';
+$lang['pre_alltokens']='All Tokens';
+$lang['pre_created']='Created';
+$lang['pre_participating']='Participating';
+$lang['pre_participated']='This round is ended.';
+$lang['pre_calc']='Calculate';
+$lang['pre_rewarding']='Rewarding';
+$lang['pre_finish']='Finish';
+$lang['pre_satges']='Stages';
+$lang['pre_txdetails']='Transaction Details';
+$lang['pre_rewarddetails']='Reward Details';
+$lang['pre_copy']='Copy Address';
 
