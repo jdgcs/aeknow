8badcf9c08b62f2d0387590db0964909b181eaae
aedev <outcrop@163.com>
2019-11-09 00:10:26 +0800

    enable cache

--- application/controllers/Address.php
+++ application/controllers/Address.php
@@ -205,7 +205,7 @@ public function wallet($ak=NULL,$page=1,$type='all'){
 		$this->load->model('Wallets');	
 		$data=$this->Wallets->getWalletInfo($ak,$page,$type);
 		$this->load->view('en/account.html',$data);
-		//$this->output->cache(2);
+		$this->output->cache(2);
 		}	
 		
 	public function accountimg($ak=NULL){
