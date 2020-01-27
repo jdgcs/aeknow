c4b093d10c247974bfd370f4ddcaccc7eb518e5f
Liu Yang <outcrop@163.com>
2020-01-27 15:54:50 +0530

    polish

--- application/models/Aenses.php
+++ application/models/Aenses.php
@@ -7,12 +7,13 @@ class Aenses extends CI_Model {
 		public function showAENSexpiring(){
 			$this->load->database();
 			$topheight=$this->GetTopHeight();
+			$target_height=$topheight+4800;//<10 days
 			$data['topheight']=$topheight;
 		
 						
 			//$sql="select tx FROM txs_aens where(recipient_id,block_height) in(SELECT recipient_id,max(block_height) from txs_aens WHERE block_height>161150 AND txtype='NameClaimTx' AND pointers is not NULL group by recipient_id) order by block_height desc;";
 			//$sql="select tx FROM txs_aens where(aensname,block_height) in(SELECT aensname,max(block_height) from txs_aens WHERE block_height>161150 AND txtype='NameClaimTx' AND pointers is not NULL group by aensname) order by block_height desc;";
-			$sql="select distinct(aensname),expire_height,nameowner from txs_aens WHERE expire_height is not null order by expire_height;";
+			$sql="select distinct(aensname),expire_height,nameowner from txs_aens WHERE expire_height is not null AND expire_height<$target_height order by expire_height;";
 			$query = $this->db->query($sql);
 			$data['expiring']="";
 			$data['expired']="";
@@ -23,9 +24,10 @@ class Aenses extends CI_Model {
 				$expire_height=$row->expire_height;
 				$nameowner=$row->nameowner;
 				$nameowner="<a href=/address/wallet/$nameowner>$nameowner</a>";
-				$biddetails="<a href=/aens/viewbids/aensname>Bid detail</a>";
+				$biddetails="<a href=/aens/viewbids/$aensname>Bid detail</a>";
+				$est_days=round(($expire_height-$topheight)/480,2);
 				if($expire_height>$topheight){
-				$data['expiring'].="<tr><td>$expire_height</td><td>$aensname</td><td>$nameowner</td><td>$biddetails</td></tr>\n";
+				$data['expiring'].="<tr><td>$expire_height(~ $est_days days)</td><td>$aensname</td><td>$nameowner</td><td>$biddetails</td></tr>\n";
 			}else{
 				$data['expired'].="<tr><td>$expire_height</td><td>$aensname</td><td>$nameowner</td><td>$biddetails</td></tr>\n";
 				}
