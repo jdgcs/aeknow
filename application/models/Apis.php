102dc040a2330ad1835a7067c72a8aee1586330f
Liu Yang <outcrop@163.com>
2020-02-12 15:57:43 +0800

    use json

--- application/models/Apis.php
+++ application/models/Apis.php
@@ -34,13 +34,18 @@ class Apis extends CI_Model {
 		$sql="SELECT distinct aensname,expire_height FROM txs_aens WHERE nameowner='$ak' order by expire_height";	
 		$query = $this->db->query($sql);
 		$counter=0;
-		
+		$str="{";		
+
 		foreach ($query->result() as $row){
 			$aens[$row->aensname]=$row->expire_height;
+			$str.='{"aensname":"'.$row->aensname.'","expire_height":'.$row->expire_height.'},';
 			//$aens[$counter]['expire_height']=$row->expire_height;
 			}
-			
-		$data=$this->object_array($aens);
+		$str="}END";
+		$str=str_replace(",}END","}",$str);
+		//$data['count']=$counter;
+		//$data=$this->object_array($aens);
+		$data=json_decode($str);
 		
 		return $data;
 		
