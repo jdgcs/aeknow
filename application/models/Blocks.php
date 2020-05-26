3a91da42abedfba5f90b46c5079c8cb42d7f50bb
Liu Yang <outcrop@163.com>
2020-05-26 11:59:11 +0800

    add link of contracts tx

--- application/models/Blocks.php
+++ application/models/Blocks.php
@@ -251,8 +251,8 @@ class Blocks extends CI_Model {
 							}
 							
 						if($key_tx=="contract_id"){
-							$comtarct_info=$this->getContractinfo($txData->tx->contract_id);
-							$content_tx=$content_tx.$comtarct_info;
+							$contarct_info=$this->getContractinfo($txData->tx->contract_id);
+							$content_tx="<a href=/contract/detail/$content_tx>$content_tx.$contarct_info</a>";
 							}
 						if($key_tx=="name_id"){
 							$name_info=$this->getNameinfo($txData->tx->name_id);
