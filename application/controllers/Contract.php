fc6764905db21af96e005df9ecfda8fa0a3dff2d
Liu Yang <outcrop@163.com>
2020-05-26 09:54:09 +0800

    use new template

--- application/controllers/Contract.php
+++ application/controllers/Contract.php
@@ -14,7 +14,7 @@ class Contract extends CI_Controller {
 	{	
 		$this->load->model('contracts');	
 		$data=$this->contracts->getContractDetail($cthash);
-		$this->load->view('contracts_detail.html',$data);
+		$this->load->view('contracts_detail_v5.html',$data);
 		$this->output->cache(3);
 	}
 	
