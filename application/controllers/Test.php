3b5e4e39b5182be5fdf81d23bcbe978184230906
Liu Yang <outcrop@163.com>
2020-06-13 17:39:13 +0800

    Access-Control-Allow-Origin

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -46,6 +46,7 @@ class Test extends CI_Controller {
 		$this->load->model('tests');
 		$data['info']=$this->tests->getTokenTable($ak);
 		$this->load->view('en/blank.html',$data);
+		$this->output->set_header("Access-Control-Allow-Origin: * ");
 		//$this->output->cache(2);
 		}	
 				
