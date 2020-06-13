b1eb6686ae04c27cd33dbed61576759351cf71a4
Liu Yang <outcrop@163.com>
2020-06-13 21:30:21 +0800

    ORDER BY alais

--- application/models/Apis.php
+++ application/models/Apis.php
@@ -48,7 +48,7 @@ class Apis extends CI_Model {
 	
 	public function getTokenTable($ak,$caller){//provide token api to users.
 		$this->load->database();		
-		$sql="SELECT * from token where account='$ak'";	
+		$sql="SELECT * from token where account='$ak' ORDER BY alais";	
 		$query = $this->db->query($sql);
 		$counter=0;
 		$str='<table class="table no-margin">
