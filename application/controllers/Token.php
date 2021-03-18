41b83dcff940310eab274194ada71d03afea24ff
U-DESKTOP-2UT82DK\outcr <outcrop@163.com>
2021-03-18 12:49:19 +0800

    add token channel

--- application/controllers/Token.php
+++ application/controllers/Token.php
@@ -4,8 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Token extends CI_Controller {
 	
 	public function index(){
-		echo "AEX9 Token Page";
+		$this->load->model('tokens');	
+		$data=$this->tokens->getTokenList();
+		$this->load->view('token_index.html',$data);
+		//$this->output->cache(5);
 		}	
+		
+		
 	//check the information of token
 	public function checktoken(){		
 		$token=trim(strtoupper($this->input->post('tokenname')));	
--- application/models/Tokens.php
+++ application/models/Tokens.php
@@ -2,6 +2,27 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Tokens extends CI_Model {
+
+public function getTokenList(){
+	$this->load->database();
+	$sql="SELECT * FROM contracts_token WHERE cert=true order by alias";
+	$query = $this->db->query($sql);
+	$data['tokens']="";
+	foreach ($query->result() as $row){
+		$tokenname=$row->alias;
+		$remark=$row->remark;
+		$supply=$row->supply;
+		$holders=$row->holders;
+		$holders="counting";
+		$transactions=$row->calltime;
+		$lastcall=$row->lastcall;
+		$data['tokens'] .="<tr><td>$tokenname</td><td>$remark</td><td>$supply</td><td>$holders</td><td>$transactions</td><td>$lastcall</td></tr>";
+		
+		}
+	
+	return $data;
+	}	
+	
 	
 public function CheckToken($token){
 	$this->load->database();
